a6179c85db07b625504eef827f2b7f4a5d7fff9f
ProsperieEli <ElijahProsperie@gmail.com>
2022-02-02 18:00:01 -0500

    finished with test

--- exercise-10/exercise-10.sql
+++ exercise-10/exercise-10.sql
@@ -1,3 +1,25 @@
 -- CUBE of amount made per year, month, day, and film rating
 -- similar to exercise 9 but include the rating with joins
 -- replace ROLLUP with CUBE
+SELECT 
+  EXTRACT (YEAR FROM payment_date) AS y,
+  EXTRACT (MONTH FROM payment_date) AS m,
+  EXTRACT (DAY FROM payment_date) AS d,
+  SUM(payment.amount),
+  film.rating
+FROM 
+  payment
+  LEFT JOIN rental
+  ON
+  rental.rental_id = payment.rental_id
+  LEFT JOIN inventory
+  ON
+  rental.inventory_id = inventory.inventory_id
+  LEFT JOIN film
+  ON
+  inventory.film_id = film.film_id
+GROUP BY 
+  CUBE (y, m, d, film.rating)
+  ORDER BY
+  film.rating, y, m, d 
+;
--- exercise-3/exercise-3.sql
+++ exercise-3/exercise-3.sql
@@ -1,3 +1,19 @@
 -- the average rental amount by country
 -- include the country name and avg amount
 -- use AVG to average the amount
+SELECT country,
+AVG(payment.amount)
+FROM country
+LEFT JOIN city
+ON city.country_id = country.country_id
+LEFT JOIN address
+ON address.city_id = city.city_id
+INNER JOIN customer
+ON 
+customer.address_id = address.address_id
+INNER JOIN payment
+ON
+customer.customer_id = payment.customer_id
+GROUP BY country
+ORDER BY avg desc
+LIMIT 10;
